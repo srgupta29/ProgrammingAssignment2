7c4306db10fc723f708c62774537ecd28f043f52
srishti <srishtigupta07@gmail.com>
2017-11-06 22:52:22 +0530

    this function returns the inverse of a matrix. It only computes the inverse once for a matrix with a given matrix object value

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function creates a special matrix and returns a list of function 
+##that can be applied on that matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+      inv <- matrix(NA)
+      set <- function (y){
+        x <<- y 
+        inv <<- matrix(NA)
+      }
+      get <- function() x
+      setinv <- function(inv_mat) inv <<- inv_mat
+      getinv <- function() inv
 
+      list(set = set , get = get , setinv = setinv , getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## this function operates on special matrix created by "makeCacheMatrix" , 
+## and returns the cached inverse of the matrix if available 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(all(!is.na(inv))){
+          return (inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat,...)
+        x$setinv(inv)
+        inv
 }
